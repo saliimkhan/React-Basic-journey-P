0c35cdf24d671aadb3f04b7b512d37df3622ce56
saliimkhan <salimkhan41111@gmail.com>
2024-03-28 05:55:46 +0530

    Day-1

---
+++ react-app/src/footer.jsx
@@ -0,0 +1,16 @@
+
+
+
+
+
+function footer() {
+ const date = new Date().getFullYear()
+
+    return (
+      <>
+        <h1>&copy; {date} hello footer </h1>
+      </>
+    )
+  }
+  
+  export default footer
---
+++ react-app/src/header.jsx
@@ -0,0 +1,17 @@
+
+
+
+
+function Header() {
+ 
+
+    return (
+      <>
+        <h1>hello header</h1>
+
+        <hr />
+      </>
+    )
+  }
+  
+  export default Header
